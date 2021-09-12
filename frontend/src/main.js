b91a36bf48e87cb30a47d044b05f88e0318b898d
Luna Wen <lunawen33@gmail.com>
2021-09-12 16:59:53 +1000

    hook up vue with graphql client

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000",
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
+// use middleware
+Vue.use(VueApollo);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  // inject apolloClient here like vue-router or vuex
+  apolloProvider,
+}).$mount("#app");
